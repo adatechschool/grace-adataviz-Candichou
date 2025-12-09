d6eebf86f068a170e7a5b3d86383083b0712a4a9
/ <candice.chevret@gmail.com>
2025-12-09 14:55:36 +0100

    Création bouton GO

--- adataviz-candichou/src/main.js
+++ adataviz-candichou/src/main.js
@@ -1,114 +1,122 @@
 /* https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/laboratoires-danalyses-medicales/records?limit=20 */
+const app = document.getElementById("app");
+let offset = 0;
+const limit = 20;
+let allLaboratories = [];
+
+// --- Barre de recherche ---
+const searchContainer = document.createElement("div");
+searchContainer.classList.add("search-container");
+const searchInput = document.createElement("input");
+searchInput.type = "text";
+searchInput.placeholder = "Rechercher par arrondissement (ex: 75015 ou 15)...";
+searchContainer.appendChild(searchInput);
+app.appendChild(searchContainer);
+
+// ----- le contenant de la liste de tous les laboratoires ---
+const listContainer = document.createElement("ul");
+app.appendChild(listContainer);
+
+//----- Création bouton VOIR PLUS-----
+const btnLoadMore = document.createElement("button");
+btnLoadMore.textContent = "voir plus";
+app.appendChild(btnLoadMore);
+
+// --- Affichage des labos ---
+function displayLaboratories(laboratories) {
+  listContainer.innerHTML = "";
+
+  if (!laboratories.length) {
+    const msg = document.createElement("p");
+    msg.textContent = "Aucun laboratoire à afficher.";
+    listContainer.appendChild(msg);
+    return;
+  }
+  laboratories.forEach((laboratory) => {
+    const listLabo = document.createElement("li");
+    const title = document.createElement("h2");
+    const name = document.createElement("p");
+    const adresse = document.createElement("p");
+    const telephone = document.createElement("p");
+    const horaires = document.createElement("p");
+
+    const containerActions = document.createElement("div");
+    const btnSchedules = document.createElement("button");
+    const btnGo = document.createElement("button");
+
+    title.textContent = laboratory.liste;
+    name.textContent = laboratory.laboratoire;
+    adresse.textContent = `${laboratory.adresse}, ${laboratory.code_postal}`;
+    telephone.textContent = laboratory.telephone;
+    horaires.textContent = laboratory.horaires;
+    btnGo.textContent = "GO";
+
+    // ----- Création du bouton HORAIRES ---
+    btnSchedules.innerText = "voir les horaires";
+    horaires.style.display = "none";
+    btnSchedules.addEventListener("click", () => {
+      if (horaires.style.display === "none") {
+        horaires.style.display = "block";
+        btnSchedules.innerText = "Voir moins";
+      } else {
+        horaires.style.display = "none";
+        btnSchedules.innerText = "voir les horaires";
+      }
+    });
+
+    listLabo.appendChild(title);
+    listLabo.appendChild(name);
+    listLabo.appendChild(adresse);
+    listLabo.appendChild(telephone);
+    listLabo.appendChild(horaires);
+
+    listLabo.appendChild(containerActions);
+    containerActions.classList.add("card-actions");
+
+    containerActions.appendChild(btnSchedules);
+    containerActions.appendChild(btnGo);
+
+    listContainer.appendChild(listLabo);
+  });
+}
 
 async function fetchLaboratories() {
-  const app = document.getElementById("app");
-
   try {
     const response = await fetch(
-      "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/laboratoires-danalyses-medicales/records?limit=20"
+      `https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/laboratoires-danalyses-medicales/records?limit=${limit}&offset=${offset}`
     );
     const apiData = await response.json();
-    const allLaboratories = apiData.results;
+    const newLaboratories = apiData.results || [];
 
-    // --- Barre de recherche ---
-    const searchContainer = document.createElement("div");
-    searchContainer.classList.add("search-container");
+    allLaboratories = [...allLaboratories, ...newLaboratories];
 
-    const searchInput = document.createElement("input");
-    /* const searchButton = document.createElement("button"); */
-
-    searchInput.type = "text";
-    searchInput.placeholder =
-      "Rechercher par arrondissement (ex: 75015 ou 15)...";
-
-    app.appendChild(searchContainer);
-    searchContainer.appendChild(searchInput);
-    /*  searchContainer.appendChild(searchButton); */
-
-    const listContainer = document.createElement("ul");
-    app.appendChild(listContainer);
-
-    //Création bouton voir plus
-    const btnLoadMore = document.createElement("button");
-    btnLoadMore.textContent = "voir plus";
-    app.appendChild(btnLoadMore);
-
-    // --- Affichage des labos ---
-    function displayLaboratories(laboratories) {
-      listContainer.innerHTML = "";
-      /* 
-      if (!laboratories || laboratories.length === 0) {
-        const empty = document.createElement("p");
-        empty.textContent =
-          "Aucun laboratoire ne correspond à cette recherche.";
-        listContainer.appendChild(empty);
-        return;
-      } */
-      /*     const loadMoreContainer = document.createElement("div");
-      loadMoreContainer.classList.add("loadMore-container"); */
-
-      laboratories.forEach((laboratory) => {
-        const listLabo = document.createElement("li");
-        const title = document.createElement("h2");
-        const name = document.createElement("p");
-        const adresse = document.createElement("p");
-        const telephone = document.createElement("p");
-        const horaires = document.createElement("p");
-        const btnSeemore = document.createElement("button");
-
-        title.textContent = laboratory.liste;
-        name.textContent = laboratory.laboratoire;
-        adresse.textContent = `${laboratory.adresse}, ${laboratory.code_postal}`;
-        telephone.textContent = laboratory.telephone;
-        horaires.textContent = laboratory.horaires;
-
-        // bouton horaires
-        btnSeemore.innerText = "voir les horaires";
-        horaires.style.display = "none";
-
-        btnSeemore.addEventListener("click", () => {
-          if (horaires.style.display === "none") {
-            horaires.style.display = "block";
-            btnSeemore.innerText = "Voir moins";
-          } else {
-            horaires.style.display = "none";
-            btnSeemore.innerText = "voir les horaires";
-          }
-        });
-
-        listLabo.appendChild(title);
-        listLabo.appendChild(name);
-        listLabo.appendChild(adresse);
-        listLabo.appendChild(telephone);
-        listLabo.appendChild(horaires);
-        listLabo.appendChild(btnSeemore);
-
-        listContainer.appendChild(listLabo);
-      });
-    }
-
-    // affichage initial
     displayLaboratories(allLaboratories);
-
-    // --- Fonction de filtrage réutilisable ---
-    function applyFilter() {
-      const valueInput = searchInput.value.trim();
-
-      const filteredLabs = allLaboratories.filter((laboratory) => {
-        const cp = String(laboratory.code_postal || "").trim();
-
-        return cp.startsWith(valueInput) || cp.includes(valueInput);
-      });
-
-      /* console.log("Résultats filtrés :", filteredLabs.length); */
-      displayLaboratories(filteredLabs);
-    }
-
-    // recherche en tapant
-    searchInput.addEventListener("input", applyFilter);
   } catch (error) {
     console.error(error);
   }
 }
+// --- Fonction de filtrage réutilisable ---
+function applyFilter() {
+  const valueInput = searchInput.value.trim();
+  if (valueInput === "") {
+    // si rien dans la barre → on affiche tout
+    displayLaboratories(allLaboratories);
+    return;
+  }
+  const filteredLabs = allLaboratories.filter((laboratory) => {
+    const cp = String(laboratory.code_postal || "").trim();
+    return cp.startsWith(valueInput) || cp.includes(valueInput);
+  });
+
+  displayLaboratories(filteredLabs);
+}
+// recherche en tapant
+searchInput.addEventListener("input", applyFilter);
+
+// Action du bouton VOIR PLUS
+btnLoadMore.addEventListener("click", () => {
+  offset += limit;
+  fetchLaboratories();
+});
 
 fetchLaboratories();
